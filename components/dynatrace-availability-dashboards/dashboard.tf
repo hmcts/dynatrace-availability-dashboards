73918292f24fcf20440117b6483b98ae62fcfcfd
JayDoubleu <git.jaydoubleu@gmail.com>
2022-11-24 19:09:21 +0000

    Move unknowns location

--- components/dynatrace-availability-dashboards/dashboard.tf
+++ components/dynatrace-availability-dashboards/dashboard.tf
@@ -4,10 +4,10 @@ resource "dynatrace_dashboard" "availability" {
     shared = true
     owner  = "platops"
     tags   = ["environment=${var.env}"]
+    unknowns = jsonencode({
+      popularity = 10
+    })
   }
-  unknowns = jsonencode({
-    popularity = 1
-  })
   dynamic "tile" {
     iterator = slo
     for_each = dynatrace_slo.availability
