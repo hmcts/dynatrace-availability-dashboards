d96215f9fc32600a3e3b4eb70659f16a7bf035ed
JayDoubleu <git.jaydoubleu@gmail.com>
2022-11-29 08:44:41 +0000

    Add initial autogeneration script

---
+++ scripts/generate_synthetic_monitors.py
@@ -0,0 +1,135 @@
+import os
+import sys
+import json
+import logging
+from datetime import datetime
+import argparse
+from helpers import (
+    Dumper,
+    get_kubectl_ingress,
+    filter_ingress,
+    read_yaml,
+    format_dt_monitors_yaml,
+    merge_synthetic_monitors_yaml,
+    merge_management_zones,
+)
+
+logger = logging.getLogger(__name__)
+datenow = datetime.now().strftime("%Y_%m_%d-%I_%M_%S")
+logfile_path = f"/tmp/{os.path.basename(__file__)}_{datenow}.log"
+
+dt_config_dir = os.path.abspath(os.path.dirname(__file__) + "/../dynatrace")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s: %(message)s",
+    handlers=[
+        logging.FileHandler(filename=logfile_path),
+        logging.StreamHandler(stream=sys.stdout),
+    ],
+)
+logger.info(f"Logging to {logfile_path}")
+
+parser = argparse.ArgumentParser(
+    description="""Convert kubernetes ingress definitions
+                 into dynatrace synthetic monitors"""
+)
+parser.add_argument(
+    "-e",
+    "--environment",
+    type=str,
+    help="Specify kubernetes cluster environment",
+    required=True,
+)
+parser.add_argument(
+    "-d",
+    "--department",
+    type=str,
+    help="Specify the departement such as CFT/SDS",
+    required=True,
+    default="CFT",
+)
+parser.add_argument(
+    "-c",
+    "--context",
+    type=str,
+    help="Specify the name of kubernetes context to use",
+    default=False,
+    required="--stdin" not in sys.argv,
+)
+parser.add_argument(
+    "--stdin",
+    help="Accept JSON data from stdin instead of running kubernetes module",
+    default=False,
+    required=False,
+    action="store_true",
+)
+
+args = parser.parse_args()
+
+environment = args.environment
+department = args.department
+context = args.context
+stdin = args.stdin
+
+monitors_yaml_path = (
+    f"{dt_config_dir}/synthetic_monitors/synthetic_monitors_{environment.lower()}.yaml"
+)
+management_zones_yaml_path = (
+    f"{dt_config_dir}/management_zones/management_zones_{environment.lower()}.yaml"
+)
+
+
+def main():
+    logger.info(f"Environment is {environment}")
+    logger.info("Trying to retrieve ingress objects from {context}...")
+    kubectl_data = get_kubectl_ingress(stdin, context)
+    kubectl_data_filtered = filter_ingress(kubectl_data)
+    # Handle synthetic monitors
+    logger.info("Handling synthetic monitors...")
+    generated_yaml_monitors = format_dt_monitors_yaml(
+        department, kubectl_data_filtered, environment
+    )
+    existing_yaml_monitors = read_yaml(monitors_yaml_path)["synthetic_monitors"]
+    (
+        monitors_final_yaml,
+        monitors_count,
+        generated_management_zones,
+    ) = merge_synthetic_monitors_yaml(existing_yaml_monitors, generated_yaml_monitors)
+
+    with open(monitors_yaml_path, "w") as f:
+        try:
+            f.write(monitors_final_yaml)
+            logger.info(
+                f"Number of synthetic monitors before: {len(existing_yaml_monitors)}"
+            )
+            logger.info(f"Number of synthetic monitors after: {monitors_count}")
+            logger.info(f"New {monitors_count} objects written to {monitors_yaml_path}")
+        except Exception as e:
+            logger.exception(e)
+
+    # Handle management zones
+    logger.info("Handling management_zones...")
+    existing_yaml_management_zones = read_yaml(management_zones_yaml_path)[
+        "management_zones"
+    ]
+    management_zones_final_yaml, management_zones_count = merge_management_zones(
+        existing_yaml_management_zones, generated_management_zones
+    )
+
+    with open(management_zones_yaml_path, "w") as f:
+        try:
+            f.write(management_zones_final_yaml)
+            logger.info(
+                f"Number of management zones before: {len(existing_yaml_management_zones)}"
+            )
+            logger.info(f"Number of management_zones after: {management_zones_count}")
+            logger.info(
+                f"New {management_zones_count} objects written to {management_zones_yaml_path}"
+            )
+        except Exception as e:
+            logger.exception(e)
+
+
+if __name__ == "__main__":
+    main()
