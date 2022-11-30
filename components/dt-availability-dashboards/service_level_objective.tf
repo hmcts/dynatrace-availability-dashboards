5a4b302984f975ba2443a8679a0ff940bdea42e5
JayDoubleu <git.jaydoubleu@gmail.com>
2022-11-30 23:29:05 +0000

    Test time_sleep per management_zone

--- components/dt-availability-dashboards/service_level_objective.tf
+++ components/dt-availability-dashboards/service_level_objective.tf
@@ -1,23 +1,26 @@
-resource "time_sleep" "wait_for_management_zones" {
-  depends_on       = [dynatrace_management_zone.availability]
-  create_duration  = "90s"
-  destroy_duration = "90s"
+resource "time_sleep" "management_zones" {
+  for_each = {
+    for management_zone in dynatrace_management_zone.availability :
+    management_zone.name => management_zone
+  }
+  create_duration  = "15s"
+  destroy_duration = "15s"
   triggers = {
-    zones = filebase64sha256("${path.module}/${local.config_path}/management_zones/management_zones_${var.env}.yaml")
+    name = each.value.name
   }
 }
 
 resource "dynatrace_slo" "availability" {
   // SLOs cannot be created before management zones, which can take some time to create
   for_each = {
-    for management_zone in dynatrace_management_zone.availability :
-    management_zone.name => management_zone
+    for management_zone in time_sleep.management_zones :
+    management_zone.triggers.name => management_zone
   }
-  name              = "[${each.value.name}] Availability"
+  name              = "[${each.value.triggers.name}] Availability"
   disabled          = false
   metric_expression = "builtin:synthetic.http.availability.location.total:splitBy()"
   evaluation        = "AGGREGATE"
-  filter            = "type(HTTP_CHECK), mzName(${each.value.name})"
+  filter            = "type(HTTP_CHECK), mzName(${each.value.triggers.name})"
   target            = 95
   warning           = 97.5
   timeframe         = "-1d"
