a18652e1d20bc0ac94437faeae1776409c0a8fce
JayDoubleu <git.jaydoubleu@gmail.com>
2022-12-07 16:30:22 +0000

    Add whitespaces to terraform output

--- components/dt-availability-dashboards/locals.tf
+++ components/dt-availability-dashboards/locals.tf
@@ -2,5 +2,5 @@ locals {
   config_path        = "../../dynatrace"
   management_zones   = yamldecode(file("${path.module}/${local.config_path}/management_zones/management_zones_${var.env}.yaml"))["management_zones"]
   synthetic_monitors = yamldecode(file("${path.module}/${local.config_path}/synthetic_monitors/synthetic_monitors_${var.env}.yaml"))["synthetic_monitors"]
-  dashboard_url      = "${var.dt_env_url}/#dashboard;id=${dynatrace_dashboard.availability.id}  "
+  dashboard_url      = "${var.dt_env_url}/#dashboard;id=${dynatrace_dashboard.availability.id};"
 }
