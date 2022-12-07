4be7e7c5c084742919032daae642ac10f93ed115
JayDoubleu <git.jaydoubleu@gmail.com>
2022-12-07 15:50:48 +0000

    Test vso output

--- components/dt-availability-dashboards/outputs.tf
+++ components/dt-availability-dashboards/outputs.tf
@@ -1,4 +1,11 @@
+locals {
+  dashboard_url = "${var.dt_env_url}/#dashboard;id=${dynatrace_dashboard.availability.id}"
+}
 output "dashboard_url" {
   description = "Dynatrace Dashboard URL"
-  value       = "${var.dt_env_url}/#dashboard;id=${dynatrace_dashboard.availability.id}"
+  value       = local.dashboard_url
+}
+output "ado_dashboard_url" {
+  description = "ADO Warning - Dynatrace Dashboard URL"
+  value       = "##vso[task.logissue type=warning] ${var.env} - Dashboard URL: ${local.dashboard_url}"
 }
