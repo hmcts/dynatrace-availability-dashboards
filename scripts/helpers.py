440fdf34ff0b0c3595f7e8324fe743529b9dca59
Rees Pozzi <reespozzi@icloud.com>
2023-01-23 13:47:47 +0000

    Remove whitespace

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -106,7 +106,7 @@ def filter_ingress(data, environment):
         if (
             environment == "demo"
             and "ingressClassName" in item["spec"]
-            and item["spec"]["ingressClassName"] 
+            and item["spec"]["ingressClassName"]
             in ["traefik-no-proxy", "traefik-private"]
         )
         or (
