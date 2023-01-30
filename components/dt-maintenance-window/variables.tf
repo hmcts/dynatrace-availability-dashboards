28590c66b5d008a0bcd65b7dccb5cfd9ff76749e
Rees Pozzi <reespozzi@icloud.com>
2023-01-30 12:42:41 +0000

    Update to include other files in component

---
+++ components/dt-maintenance-window/variables.tf
@@ -0,0 +1,22 @@
+variable "product" {
+  type        = string
+  default     = null
+  description = "product variable required by cnp terraform template. Not in use by terraform"
+}
+variable "builtFrom" {
+  type        = string
+  default     = null
+  description = "builtFrom variable required by cnp terraform template. Not in use by terraform"
+}
+variable "env" {
+  type        = string
+  description = "Name of the environment to build in"
+}
+variable "dt_env_url" {
+  type        = string
+  description = "Dynatrace environment URL"
+}
+variable "dynatrace_platops_api_key" {
+  type        = string
+  description = "Dynatrace API access key"
+}
