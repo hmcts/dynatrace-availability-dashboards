4a58c0ec8cfc4ae4deeee0b5b419b077045638e9
Rees Pozzi <rees.pozzi@justice.gov.uk>
2023-02-01 14:06:18 +0000

    Disable HTTP monitor problem alerting (#246)
    * Disable HTTP monitor problem alerting
    
    We aren't proactively doing anything with generated problems with our SLOs at the minute, but they're generating SNOW incidents and clogging up Dynatrace with alerts.
    
    Once (if) we have an action plan with these problems, we can re-enable this.
    HTTP monitors are still running and adding to SLO numbers

--- components/dt-availability-dashboards/synthetic_monitor.tf
+++ components/dt-availability-dashboards/synthetic_monitor.tf
@@ -16,11 +16,8 @@ resource "dynatrace_http_monitor" "availability" {
       enabled = true
     }
     outage_handling {
-      global_outage = true
+      global_outage = false
       local_outage  = false
-      global_outage_policy {
-        consecutive_runs = 1
-      }
     }
   }
   tags {
